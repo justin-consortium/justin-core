5f52c48b7b0c6ac5c1425e8272e369d22c1bd4ac
José-Antonio Rubio <jadrubio@umich.edu>
2026-02-25 09:58:16 -0500

    created new JUser typing

--- src/user-manager/user.type.ts
+++ src/user-manager/user.type.ts
@@ -1,10 +1,27 @@
-export type JUser = {
+/**
+ * Base fields that are always present on a Justin user.
+ *
+ * These fields are reserved and should not be overridden by
+ * application-level user data.
+ */
+export type BaseJUser = {
   id: string;
   uniqueIdentifier: string;
-  attributes: Record<string, any>;
 };
 
-export type NewUserRecord = {
+/**
+ * A Justin user record.
+ *
+ * TUserData represents all additional user fields.
+ */
+export type JUser<
+  TUserData extends Record<string, any> = Record<string, any>,
+> = BaseJUser & TUserData;
+
+
+export type NewUserRecord<
+  TUserData extends Record<string, any> = Record<string, any>,
+> = {
   uniqueIdentifier: string;
-  initialAttributes: Record<string, any>;
+  initialAttributes?: Omit<TUserData, keyof BaseJUser>;
 };
