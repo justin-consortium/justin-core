96cde34280775b10c23eb9a50ea4fe0983225264
José-Antonio Rubio <jadrubio@umich.edu>
2026-03-11 12:47:06 -0400

    updated index.ts for exporting out of package

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+export { DataManager, USERS, PROTECTED_ATTRIBUTES, DBType, NO_ID, ChangeListenerManager, CollectionChangeType } from './data-manager';
+export type { DataManagerAdapter, CollectionChangeListener, CollectionChangeNotifier } from './data-manager';
 
-export { DataManager, USERS, PROTECTED_ATTRIBUTES, DBType, NO_ID, ChangeListenerManager, CollectionChangeType  } from './data-manager';
-
+/**
+ * Errors
+ */
+export { JustinError, JustinErrorCode } from './errors';
 
 /**
  * UserManager
  */
 export { UserManager } from './user-manager/user-manager';
-export type { JUser, NewUserRecord } from './user-manager/types';
+export type { JUser, BaseJUser, NewUserRecord, NamespacedAttributes, ProtectedAttributesRecord, BaseProtectedAttributes } from './user-manager/types';
 
 /**
  * Logging
