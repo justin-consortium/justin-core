4d649fa5d8c7ea64165783f111a873a597c3e043
José-Antonio Rubio <jadrubio@umich.edu>
2026-04-02 15:18:10 -0400

    lint fixer

--- src/data-manager/types.ts
+++ src/data-manager/types.ts
@@ -1,4 +1,5 @@
 import type { Readable } from 'stream';
+import type { DBType } from './constants';
 
 export enum CollectionChangeType {
   INSERT = 'insert',
@@ -24,7 +25,7 @@ export enum CollectionChangeType {
  */
 export type DBConfig = {
   /** Database type. Currently only `DBType.MONGO` is supported. */
-  dbType: import('./constants').DBType;
+  dbType: DBType;
   /** Connection string for the database. */
   uri: string;
   /** Database name. Falls back to the adapter default if omitted. */
