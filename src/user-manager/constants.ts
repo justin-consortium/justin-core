5118e50b300e0998b6d2f78746ab558a32d8db43
José-Antonio Rubio <jadrubio@umich.edu>
2026-04-28 12:14:13 -0400

    removed `as const` from user-manager constants

--- src/user-manager/constants.ts
+++ src/user-manager/constants.ts
@@ -1,4 +1,4 @@
-const USERS = 'users' as const;
-const PROTECTED_ATTRIBUTES = 'protected_attributes' as const;
+const USERS = 'users';
+const PROTECTED_ATTRIBUTES = 'protected_attributes';
 
 export { PROTECTED_ATTRIBUTES, USERS };
